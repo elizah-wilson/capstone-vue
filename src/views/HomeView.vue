e722b48d00089bcd758162effc1639d457a0c53d
Devan Allara <DAllara@humana.com>
2024-01-11 14:53:58 -0500

    made it work (kinda)

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,71 +1,94 @@
 <script setup>
 import $ from 'jquery'
+import { ref, onMounted } from 'vue'
 
-(function ($) {
 
+onMounted(() => {
+  (function( $ ) {
+  
   console.clear()
   console.log('svgColor')
+  
   var mainHolder, colorHolder
-  var btnClear
-  var svgObject, svgOutline, svgColor
+  var btnRandom, btnClear, btnDownloadSVG, btnDownloadPNG
+  var svgObject, svgOutline, svgColor 
   var swatchUp, swatchDown
   var fillSpeed = 0.15
   var chosenColor = '#FFFFFF'
-  var colors = ['#1d561c', '#699b68', '#61ce73', '#afe89a', '#e9edb2', '#efe77b', '#f4d24f', '#bc9d71', '#08316d', '#265a8b', '#5da4ba', '#7ad0d3', '#e7b6af', '#faca9a', '#fe8d7d', '#9b6959', '#552056', '#874a9e', '#b595e5', '#b33a6d', '#e2649e', '#ec8a8e', '#fd6d4a', '#7c373f']
+  var colors = ['#FFFFFF', '#8E53A1', '#6ABD46', '#71CCDC', '#F7ED45', '#F7DAAF', '#EC2527', '#F16824', '#CECCCC', '#5A499E', '#06753D', '#024259', '#FDD209', '#7D4829', '#931B1E', '#B44426', '#979797', '#C296C5', '#54B948', '#3C75BB', '#F7ED45', '#E89D5E', '#F26F68', '#F37123', '#676868', '#9060A8', '#169E49', '#3CBEB7', '#FFCD37', '#E5B07D', '#EF3C46', '#FDBE17', '#4E4D4E', '#6B449B', '#BACD3F', '#1890CA', '#FCD55A', '#D8C077', '#A62E32', '#F16A2D', '#343433', '#583E98', '#BA539F', '#9D2482', '#DD64A5', '#DB778D', '#EC4394', '#E0398C', '#68AF46', '#4455A4', '#FBEE34', '#AD732A', '#D91E36', '#F99B2A']
   var closeOffset
 
-  function swatchClick() {
+  function swatchClick(){
     chosenColor = $(this).data('color')
     console.log(chosenColor)
-    TweenMax.to(colorHolder, fillSpeed, { backgroundColor: chosenColor })
+    TweenMax.to(colorHolder, fillSpeed, { backgroundColor:chosenColor })
   }
-  function swatchMove(e) {
-    var moveTo = (e.type == 'mouseenter') ? swatchUp : swatchDown;
+  function swatchMove(e){
+    var moveTo = (e.type == 'mouseenter')? swatchUp: swatchDown;
     TweenMax.to('.swatchHolder', 0.5, moveTo);
   }
-
+  
   function colorMe() {
     TweenMax.to(this, fillSpeed, { fill: chosenColor });
   }
-
-  function colorRollover(e) {
-    var rollover = (e.type == 'mouseenter') ? { scale: 1.2 } : { scale: 1 };
-    TweenMax.to($(this), 0.05, rollover);
+  function colorRollover(e){
+    var rollover = (e.type == 'mouseenter')? {scale:1.2}:{scale:1};
+    TweenMax.to($(this), 0.05, rollover); 
   }
-
-  function download() {
+  
+  function download(){
     var svgInfo = document.querySelector('svg').outerHTML
     var parser = new DOMParser();
-    var xmlDoc = parser.parseFromString(svgInfo, "text/xml");
+    var xmlDoc = parser.parseFromString(svgInfo,"text/xml");
     var dl = document.createElement("a");
     document.body.appendChild(dl); // This line makes it work in Firefox.
-    dl.setAttribute("href", "data:image/svg+xml;base64," + xmlDoc);
-    dl.setAttribute("download", "coloringpage.svg");
+    dl.setAttribute("href", "data:image/svg+xml;base64,"+xmlDoc);
+    dl.setAttribute("download", "test.svg");
     dl.click();
   }
 
   function svgRandom() {
-    $(svgColor).each(function () {
+    $(svgColor).each(function(){
       var randomNum = Math.floor((Math.random() * colors.length) + 1);
       TweenMax.to(this, fillSpeed, { fill: colors[randomNum] });
     })
   }
   function svgClear() {
-    $(svgColor).each(function () {
+    $(svgColor).each(function(){
       TweenMax.to(this, fillSpeed, { fill: "#FFF" });
     })
   }
+  function svgDownloadSVG() {
+   var svgInfo = $(svgObject).clone();
+   console.clear()
+   console.log(svgInfo)
+   $(this).attr({
+            href:"data:image/svg+xml;base64,"+svgInfo.toString(),
+            download:'coloringBook.svg',
+            target:"_blank"
+    });
+  }
+  function svgDownloadPNG() {
+   // Future expantion:
+   // Look at https://bl.ocks.org/biovisualize/8187844
+  }
+  
 
-  $.fn.makeSwatches = function () {
-    var swatchHolder = $('<ol/>', { 'class': 'swatchHolder' }).appendTo(this)
-    colorHolder = $('<li/>', { 'class': 'colorHolder', 'text': 'Color Palette' }).css('background-color', chosenColor).appendTo(swatchHolder)
+  $.fn.makeSwatches = function() {
+    var swatchHolder = $('<ol/>', {'class': 'swatchHolder'}).appendTo(this)
+        colorHolder  = $('<li/>', {'class': 'colorHolder', 'text':'Current Color'}).css('background-color', chosenColor).appendTo(swatchHolder)
 
-    $.each(colors, function () {
+        swatchHolder.addClass('swatchHolder')
+        colorHolder.addClass('cardHolder')
+
+
+    $.each(colors, function() {
       var swatch = $('<li/>').appendTo(swatchHolder)
       $(swatch).css('background-color', this)
       $(swatch).data('color', this)
       $(swatch).on('click', swatchClick)
       $(swatch).on('mouseenter mouseleave', colorRollover)
+      $(swatch).addClass('color')
     })
 
     var swatchPos = $('.colorHolder').position()
@@ -73,15 +96,15 @@ import $ from 'jquery'
     closeOffset = swatchHeight - $('.swatchHolder').outerHeight()
 
     $('.swatchHolder').on('mouseenter mouseleave', swatchMove)
-    $('.swatchHolder').css('bottom', closeOffset)
-    swatchUp = { css: { bottom: 0 } }
-    swatchDown = { css: { bottom: closeOffset } }
-  }
-  $.fn.makeSVGcolor = function (svgURL) {
+    $('.swatchHolder').css('bottom',closeOffset)
+    swatchUp   = {css:{bottom:0}}
+    swatchDown = {css:{bottom:closeOffset}}
+  } 
+  $.fn.makeSVGcolor = function(svgURL) {
     mainHolder = this
-    $(this).load(svgURL, function () {
-      svgObject = $('svg', this)
-      svgColor = $('g#Color', svgObject).children()
+    $( this ).load(svgURL, function() {
+      svgObject  = $('svg', this)
+      svgColor   = $('g#Color', svgObject).children()
       svgOutline = $('g:nth-child(1)', svgObject).children()
       $(svgColor).on('click', colorMe)
       $(mainHolder).makeSwatches()
@@ -89,203 +112,211 @@ import $ from 'jquery'
     });
   }
 
-  $.fn.btnClear = function () {
+  $.fn.btnRandom    = function() {
+    btnRandom = this
+    $(btnRandom).on('click', svgRandom)
+  }
+  $.fn.btnClear     = function() {
     btnClear = this
     $(btnClear).on('click', svgClear)
   }
-}
+  $.fn.btnDownload  = function(type) {
+    if(type == 'PNG'){
+      btnDownloadPNG = this
+      $(this).on('mouseenter', svgDownloadPNG)
+    } else {
+      btnDownloadSVG = this
+      $(this).on('mouseenter', svgDownloadSVG)
+    }
+  }
 
-  (jQuery));
+  $.fn.btnDownload2 = function() {
+    btnClear = this
+    $(btnClear).on('click', download)
+  }
 
-$('#ActivityDIV').makeSVGcolor('https://assets.codepen.io/5936329/Coloringbook1.svg')
 
-$('#btnClear').btnClear()
+$('#ActivityDIV'   ).makeSVGcolor('https://s3-us-west-2.amazonaws.com/s.cdpn.io/40041/cheshire.svg')
+$('#btnRandom'     ).btnRandom()
+$('#btnClear'      ).btnClear()
+$('#btnDownloadSVG').btnDownload()
+$('#btnDownloadSVG2').btnDownload2()
+}( jQuery ));
+})
 
 </script>
 
 
 <template>
-
 <div class='holder'>
-  <div class='Title'>Coloring Page</div>
-  
-  <div id="imageonly">
-    
-  </div>
-  
+  <div class='Title'>Interactive Coloring Page</div>
+  <div id="imageonly"></div>
   <div class='held' id='ActivityDIV'></div>
- 
-  <div class='held'>  
+  <div class='held'>
+    <a id="btnRandom"       class="button">Random Color</a>
     <a id="btnClear"        class="button">Clear Color</a>
   </div>
-
 </div>
-
-
-
-
 </template>
 
 
-<style scoped>
-.holder {
-  width: 900px;
+<style>
+
+.holder{
+  width:900px;
   height: 1100px;
   background-image: url("https://assets.codepen.io/5936329/background-code.png");
-
+  
   border-radius: 35px;
   align-self: center;
   justify-self: center;
   -webkit-box-sizing: border-box;
   -moz-box-sizing: border-box;
   box-sizing: border-box;
-  border: 0px solid;
-
+  border: 0px solid ;
+    
   display: inline-block;
-
+ 
   margin: auto;
   padding: 2vmin;
-
-
+ 
+  
 }
 
-.imageonly {
-
-  background-image: url("https://assets.codepen.io/5936329/background-code.png");
-
+.imageonly{
+ 
+background-image: url("https://assets.codepen.io/5936329/background-code.png");
+ 
 }
 
-.Title {
-
+.Title{
+  
   font-family: 'Averia Gruesa Libre';
   font-size: 60px;
   text-align: center;
   padding: 30px;
-  Color: #b33a6d;
-
-
+  Color: #b33a6d; 
+  
+  
 }
-
-.held {
-  position: relative;
-  overflow: hidden;
+ 
+.held{ 
+  position:relative;
+  overflow:hidden;
   border-radius: 20px;
   align-self: center;
   justify-self: center;
-  -webkit-box-sizing: border-box;
+   -webkit-box-sizing: border-box;
   -moz-box-sizing: border-box;
   box-sizing: border-box;
   border: 0px;
-
+ 
   margin: auto;
   padding: 2vmin;
-
-
+ 
+  
 }
 
-.buttons {
-  display: inline-block;
-  padding: 15px;
-
+.buttons{
+  display:inline-block;
+  padding:15px;
+  
 }
 
-.button {
+.button{
   transition-duration: 0.4s;
-  width: 150px;
-  padding: 10px;
-  background: #7ad0d3;
+  width:150px;
+  padding:10px; 
+  background: #7ad0d3; 
   font: 15px Arial, sans-serif;
   height: 40px;
   line-height: 42px;
-  border: none;
-
-
-  outline: none;
-
+  border: none;  
+   
+  
+  outline:none;
+ 
   text-align: center;
-
-  border-radius: 40px;
+ 
+  border-radius:40px;
   background: #fff;
   border: px solid #b33a6d;
-  color: #b33a6d;
-  letter-spacing: 1px;
-  text-shadow: 0;
-
+  color:#b33a6d;
+  letter-spacing:1px;
+  text-shadow:0;
+  
   cursor: pointer;
   transition: all 0.25s ease;
 }
-
-
-.button:hover {
+  
+  
+ .button:hover { 
   color: white;
-  background: #b33a6d;
-}
+    background: #b33a6d;
+} 
 
-.btnClear {
+
+
+.btnClear{
   font: 15px Arial, sans-serif;
 }
-
-.btnDownloadSVG {
+.btnDownloadSVG{
   font: 15px Arial, sans-serif;
+  
+} 
 
-}
-
-#ActivityDIV {
-  padding: 65px;
+#ActivityDIV{
+  padding:65px; 
   background: white;
   border: 0px;
-
+  
 }
 
-#ActivityDIV .swatchHolder {
-  position: absolute;
-  bottom: 0px;
-  margin: auto;
-  left: 0px;
-  right: 0px;
-  list-style-type: none;
-  text-align: center;
-  letter-spacing: 1px;
-  font-family: Arial;
-  display: inline-block;
+.swatchHolder{
+  position:absolute;
+  bottom:0px;
+  margin:auto;
+  left:0px;
+  right:0px;
+  list-style-type:none;
+  text-align:center;
+  letter-spacing:1px;
+  font-family:Arial;
+  display:inline-block;
   padding: 15px;
-  width: 260px;
+  width:260px;
   border-radius: 35px 35px 0px 0px;
   color: #232323;
-  background-image: url("https://assets.codepen.io/5936329/background-code.png");
+    background-image: url("https://assets.codepen.io/5936329/background-code.png");
   border: 0px;
-
+  
 }
-
-#ActivityDIV .swatchHolder .colorHolder {
-  width: 100%;
+.colorHolder{
+  width:100%;
   line-height: 100%;
-  content: 'Color Palette';
+  content:'Color Palette';
   padding: 10px 0px;
   margin: 0px auto 15px auto;
-  cursor: pointer;
+  cursor:pointer;
   border-radius: 20px;
 }
 
-#ActivityDIV .swatchHolder li:not(.colorHolder) {
-  height: 25px;
-  width: 25px;
-  margin: 2px;
-  display: inline-block;
-  cursor: pointer;
+.color{
+  height:25px;
+  width:25px;
+  margin:2px;
+  display:inline-block;
+  cursor:pointer;
   border-radius: 20px;
-
-
 }
 
-#ActivityDIV svg {
-  width: 100%;
-  display: inline-block;
-
+#ActivityDIV svg{
+  width:100%;
+  display:inline-block;
+  
 }
 
-#ActivityDIV svg path {
-  cursor: pointer;
-
+#ActivityDIV svg path{
+  cursor:pointer;     
 }
 </style>
